bbbebcd32a9fcb4def8dd23e644355052914042c
Nikolay Dermendzhiev <dermendzhiev.n@gmail.com>
2020-02-09 22:18:07 +0200

    add new rpc calls, extend messages and add interceptors

--- src/microservices/users/src/Api/Startup.cs
+++ src/microservices/users/src/Api/Startup.cs
@@ -1,9 +1,7 @@
 namespace Api
 {
     using System;
-
-    using Application.Interfaces;
-    using Application.Services;
+    using Api.Interceptors;
 
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
@@ -44,8 +42,15 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
-            services.AddGrpc();
-            services.AddScoped<IUserService, UserService>();
+
+            services.AddGrpc(options =>
+            {
+                options.Interceptors.Add<ExceptionHandleInterceptor>();
+                options.Interceptors.Add<LoggerInterceptor>();
+
+                options.MaxReceiveMessageSize = 1 * 1024 * 1024; // 1 MB
+                options.MaxSendMessageSize = 1 * 1024 * 1024; // 1 MB
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,13 +68,14 @@ namespace Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<UserHandler>();
+                endpoints.MapGrpcService<UserHandlerV1>();
+                endpoints.MapMetrics();
 
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                    await context.Response.WriteAsync("Users service is up and running!\n" +
+                        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
                 });
-                endpoints.MapMetrics();
             });
         }
     }
