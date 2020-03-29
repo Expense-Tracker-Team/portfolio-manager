0a3db24a29a4f54c6a6c2746b11803382c6bd74c
Dermendzhiev <dermendzhiev.n@gmail.com>
2020-03-29 10:55:07 +0300

    Extend Jaeger configuration and export it in appsettings

---
+++ src/microservices/users/src/Api/Config/Jaeger/JaegerExtensions.cs
@@ -0,0 +1,75 @@
+﻿namespace Api.Config.Jaeger
+{
+    using Api.Constants;
+    using global::Jaeger;
+    using global::Jaeger.Reporters;
+    using global::Jaeger.Samplers;
+    using global::Jaeger.Senders;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using OpenTracing;
+    using OpenTracing.Util;
+
+    public static class JaegerExtensions
+    {
+        public static IServiceCollection ConfigureTracingServices(this IServiceCollection services)
+        {
+            JaegerOptions jaegerOptions = GetJaegerOptions(services);
+
+            if (!jaegerOptions.Enabled)
+            {
+                return services;
+            }
+
+            services.AddSingleton<ITracer>(serviceProvider =>
+            {
+                ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+                RemoteReporter reporter = new RemoteReporter
+                        .Builder()
+                    .WithSender(new UdpSender(jaegerOptions.UdpHost, jaegerOptions.UdpPort, jaegerOptions.MaxPacketSize))
+                    .WithLoggerFactory(loggerFactory)
+                    .Build();
+
+                ISampler sampler = GetJaegerSampler(jaegerOptions);
+
+                Tracer tracer = new Tracer
+                        .Builder(jaegerOptions.ServiceName)
+                    .WithReporter(reporter)
+                    .WithSampler(sampler)
+                    .Build();
+
+                GlobalTracer.Register(tracer);
+
+                return tracer;
+            });
+
+            services.AddOpenTracing();
+
+            return services;
+        }
+
+        private static JaegerOptions GetJaegerOptions(IServiceCollection services)
+        {
+            using ServiceProvider serviceProvider = services.BuildServiceProvider();
+            IConfiguration configuration = serviceProvider.GetService<IConfiguration>();
+            services.Configure<JaegerOptions>(configuration.GetSection(ApplicationSettingKeys.JaegerKey));
+
+            return services.BuildServiceProvider().GetRequiredService<IOptions<JaegerOptions>>().Value;
+        }
+
+        private static ISampler GetJaegerSampler(JaegerOptions options)
+        {
+            return options.Sampler switch
+            {
+                "const" => new ConstSampler(true),
+                "rate" => new RateLimitingSampler(options.MaxTracesPerSecond),
+                "probabilistic" => new ProbabilisticSampler(options.SamplingRate),
+                _ => new ConstSampler(true),
+            };
+        }
+
+    }
+}
---
+++ src/microservices/users/src/Api/Constants/ApplicationSettingKeys.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.Constants
+{
+    public class ApplicationSettingKeys
+    {
+        public const string JaegerKey = "jaeger";
+    }
+}
