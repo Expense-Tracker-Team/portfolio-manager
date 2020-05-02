a721c5701eade7359c2964398ebd0c3d5ba4c3d1
Dermendzhiev <dermendzhiev.n@gmail.com>
2020-05-02 17:43:36 +0300

    add unit tests for Api, Application and Domain

--- src/microservices/users/tests/UnitTests/Api/Tests/Handlers/UserHandlerV1Tests.cs
+++ src/microservices/users/tests/UnitTests/Api/Tests/Handlers/UserHandlerV1Tests.cs
@@ -2,17 +2,55 @@
 {
     using System.Threading.Tasks;
     using Xunit;
-    using Google.Protobuf.WellKnownTypes;
     using FluentAssertions;
-    using Grpc.Core;
     using global::Api.Protos;
     using global::Api;
+    using global::Application.UseCases.Interfaces;
+    using global::Application.UseCases.Models;
     using System;
     using UnitTests.Api.Helpers;
-    using Microsoft.Extensions.Logging;
-    using Moq;
+    using FakeItEasy;
+    using User = global::Domain.User;
 
     public class UserHandlerV1Tests
     {
+        [Fact]
+        public async Task CreateUser_WithValidParameters_ShouldReturnCreatedUser()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            const string email = "test@email.com";
+            const string password = "secret_password";
+            const string name = "John Doe";
+            const string phoneNumber = "08888888888";
+
+            ICreateUserUseCase createUserUseCaseStub = A.Fake<ICreateUserUseCase>();
+            A.CallTo(() => createUserUseCaseStub.ExecuteAsync(
+                A<CreateUserInput>.That.Matches(
+                    i => i.Email == email
+                    && i.Password == password
+                    && i.Name == name
+                    && i.PhoneNumber == phoneNumber)))
+                .Returns(new User(id, email, password, name, phoneNumber));
+
+            var userHandler = new UserHandlerV1(null, createUserUseCaseStub);
+
+            var request = new CreateUserRequest()
+            {
+                Email = email,
+                Password = password,
+                Name = name,
+                PhoneNumber = phoneNumber
+            };
+
+            // Act
+            CreateUserResponse actual = await userHandler.CreateUser(request, TestServerCallContext.Create());
+
+            // Assert
+            actual.Uuid.Should().Be(id.ToString());
+            actual.Email.Should().Be(email);
+            actual.Name.Should().Be(name);
+            actual.PhoneNumber.Should().Be(phoneNumber);
+        }
     }
 }
---
+++ src/microservices/users/tests/UnitTests/Application/UseCases/Implementations/CreateUserUseCaseTests.cs
@@ -0,0 +1,50 @@
+﻿namespace UnitTests.Application.UseCases.Implementations
+{
+    using System.Threading.Tasks;
+    using Xunit;
+    using FluentAssertions;
+    using System;
+    using FakeItEasy;
+    using global::Application.Infrastructure.Repositories;
+    using global::Application.UseCases.Implementations;
+    using global::Application.UseCases.Models;
+    using global::Domain;
+
+    public class CreateUserUseCaseTests
+    {
+        [Fact]
+        public async Task ExecuteAsync_WithValidInput_ShouldReturnCreatedUser()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            const string email = "test@email.com";
+            const string password = "secret_password";
+            const string name = "John Doe";
+            const string phoneNumber = "08888888888";
+
+            IUserRepository userRepositoryStub = A.Fake<IUserRepository>();
+            A.CallTo(() => userRepositoryStub.CreateAsync(
+                A<User>.That.Matches(
+                    i => i.Email == email
+                    && i.PasswordHash == password
+                    && i.Name == name
+                    && i.PhoneNumber == phoneNumber
+                    && i.Id != Guid.Empty)))
+                .Returns(new User(id, email, password, name, phoneNumber));
+
+            var useCase = new CreateUserUseCase(userRepositoryStub);
+
+            var input = new CreateUserInput(email, password, name, phoneNumber);
+
+            // Act
+            User actual = await useCase.ExecuteAsync(input);
+
+            // Assert
+            actual.Id.Should().Be(id);
+            actual.Email.Should().Be(email);
+            actual.PasswordHash.Should().Be(password);
+            actual.Name.Should().Be(name);
+            actual.PhoneNumber.Should().Be(phoneNumber);
+        }
+    }
+}
