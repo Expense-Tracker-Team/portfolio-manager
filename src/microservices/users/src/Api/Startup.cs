91c05200669c8a035b2c77a265df9f37dd5eaffd
Miroslav Stoyanov <miroslav.stoyanov@musala.com>
2020-05-18 11:54:13 +0300

    Add missing context DI

--- src/microservices/users/src/Api/Startup.cs
+++ src/microservices/users/src/Api/Startup.cs
@@ -23,6 +23,7 @@ namespace Api
     using Persistence.Repositories;
     using Application.UseCases.Interfaces;
     using Application.UseCases.Implementations;
+    using Persistence.Interfaces;
 
     public class Startup
     {
@@ -44,6 +45,7 @@ namespace Api
             services.AddTransient<IUserRepository, UserRepository>();
             services.AddTransient<ICreateUserUseCase, CreateUserUseCase>();
             services.AddTransient<IGetUserUseCase, GetUserUseCase>();
+            services.AddTransient<IUsersDbContext, UsersDbContext>();
 
             this.ConfigurePostgresDatabaseServices(services);
             this.ConfigureLoggingServices(services);
